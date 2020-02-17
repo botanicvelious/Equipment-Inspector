c39bdc9d47f0982807ed780887d9be3b7e3a25dd
cyberempires@gmail.com <Botanicvelious0>
2020-02-17 05:10:23 -0700

    remove unecessary duplicate switich

--- src/main/java/equipmentinspector/EquipmentInspectorPanel.java
+++ src/main/java/equipmentinspector/EquipmentInspectorPanel.java
@@ -29,7 +29,6 @@ public class EquipmentInspectorPanel extends PluginPanel
     private JPanel equipmentPanels;
     private JPanel header;
     private JLabel nameLabel;
-    private int itemid;
 
     @Inject
     private ItemManager itemManager;
@@ -88,60 +87,7 @@ public class EquipmentInspectorPanel extends PluginPanel
                     equipmentPanels.removeAll();
                     playerEquipment.forEach((kitType, itemComposition) ->
                     {
-                        switch(itemComposition.getId()) {
-                            case ABYSSAL_BRACELET1:
-                            case ABYSSAL_BRACELET2:
-                            case ABYSSAL_BRACELET3:
-                            case ABYSSAL_BRACELET4:
-                                itemid = ABYSSAL_BRACELET5;
-                                break;
-                            case BURNING_AMULET1:
-                            case BURNING_AMULET2:
-                            case BURNING_AMULET3:
-                            case BURNING_AMULET4:
-                                itemid = BURNING_AMULET5;
-                                break;
-                            case COMBAT_BRACELET:
-                            case COMBAT_BRACELET1:
-                            case COMBAT_BRACELET2:
-                            case COMBAT_BRACELET3:
-                            case COMBAT_BRACELET4:
-                            case COMBAT_BRACELET5:
-                               itemid = COMBAT_BRACELET6;
-                                break;
-                            case DIGSITE_PENDANT_1:
-                            case DIGSITE_PENDANT_2:
-                            case DIGSITE_PENDANT_3:
-                            case DIGSITE_PENDANT_4:
-                                itemid = DIGSITE_PENDANT_5;
-                                break;
-                            case AMULET_OF_GLORY1:
-                            case AMULET_OF_GLORY2:
-                            case AMULET_OF_GLORY3:
-                            case AMULET_OF_GLORY4:
-                            case AMULET_OF_GLORY5:
-                            case AMULET_OF_GLORY:
-                                itemid = AMULET_OF_GLORY6;
-                                break;
-                            case AMULET_OF_GLORY_T1:
-                            case AMULET_OF_GLORY_T2:
-                            case AMULET_OF_GLORY_T3:
-                            case AMULET_OF_GLORY_T4:
-                            case AMULET_OF_GLORY_T5:
-                            case AMULET_OF_GLORY_T:
-                                itemid = AMULET_OF_GLORY_T6;
-                                break;
-                            case RING_OF_WEALTH:
-                            case RING_OF_WEALTH_1:
-                            case RING_OF_WEALTH_2:
-                            case RING_OF_WEALTH_3:
-                            case RING_OF_WEALTH_4:
-                                itemid = RING_OF_WEALTH_5;
-                                break;
-                            default:
-                                itemid = itemComposition.getId();
-                        }
-                        AsyncBufferedImage itemImage = itemManager.getImage(itemid);
+                        AsyncBufferedImage itemImage = itemManager.getImage(itemComposition.getId());
                         equipmentPanels.add(new ItemPanel(itemComposition, kitType, itemImage), c);
                         c.gridy++;
 
