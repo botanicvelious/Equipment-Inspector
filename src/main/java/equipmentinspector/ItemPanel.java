e92e6a673fee512c142066d57cf247683d7d0f92
greatmoves <65767022+greatmoves@users.noreply.github.com>
2020-06-02 18:10:05 +1000

    added wiki support

--- src/main/java/equipmentinspector/ItemPanel.java
+++ src/main/java/equipmentinspector/ItemPanel.java
@@ -6,10 +6,14 @@ import net.runelite.client.plugins.playerindicators.PlayerNameLocation;
 import net.runelite.client.util.AsyncBufferedImage;
 import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.FontManager;
+import net.runelite.client.util.LinkBrowser;
+import okhttp3.HttpUrl;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -27,6 +31,22 @@ class ItemPanel extends JPanel
 
         JLabel name = new JLabel(item.getName());
 
+        JMenuItem wiki = new JMenuItem("Wiki");
+
+        HttpUrl WIKI_BASE = HttpUrl.parse("https://oldschool.runescape.wiki");
+        String UTM_SORUCE_KEY = "utm_source";
+        String UTM_SORUCE_VALUE = "runelite";
+
+        HttpUrl.Builder urlBuilder = WIKI_BASE.newBuilder();
+        urlBuilder.addPathSegments("w/Special:Lookup")
+                .addQueryParameter("type", "item")
+                .addQueryParameter("id", "" + item.getId())
+                .addQueryParameter("name", item.getName())
+                .addQueryParameter(UTM_SORUCE_KEY, UTM_SORUCE_VALUE);
+
+        HttpUrl url = urlBuilder.build();
+        wiki.addActionListener(e-> LinkBrowser.browse(url.toString()));
+
         JLabel location = new JLabel(StringUtils.capitalize(kitType.toString().toLowerCase()));
         location.setFont(FontManager.getRunescapeSmallFont());
 
@@ -36,6 +56,10 @@ class ItemPanel extends JPanel
         JLabel imageLabel = new JLabel();
         icon.addTo(imageLabel);
 
+        JPopupMenu popupMenu = new JPopupMenu();
+        popupMenu.setBorder(new EmptyBorder(5, 5, 5, 5));
+        popupMenu.add(wiki);
+
         layout.setVerticalGroup(layout.createParallelGroup()
                 .addComponent(imageLabel)
                 .addGroup(layout.createSequentialGroup()
@@ -43,6 +67,7 @@ class ItemPanel extends JPanel
                         .addComponent(location)
                         .addComponent(price)
                 )
+
         );
 
         layout.setHorizontalGroup(layout.createSequentialGroup()
@@ -53,10 +78,13 @@ class ItemPanel extends JPanel
                         .addComponent(location)
                         .addComponent(price)
                 )
+
         );
+
 
         // AWT's Z order is weird. This put image at the back of the stack
         setComponentZOrder(imageLabel, getComponentCount() - 1);
+        setComponentPopupMenu(popupMenu);
     }
 
 }
