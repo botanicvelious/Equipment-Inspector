66f812fcbfcdc9c01354da725fa1b3bdc692f3f9
Waistcoat <Waistcoat@github.com>
2024-01-15 15:12:34 -0700

    Adding hold shift option to equipment inspector

--- src/main/java/equipmentinspector/EquipmentInspectorConfig.java
+++ src/main/java/equipmentinspector/EquipmentInspectorConfig.java
@@ -7,5 +7,14 @@ import net.runelite.client.config.ConfigItem;
 @ConfigGroup("equipmentinspector")
 public interface EquipmentInspectorConfig extends Config
 {
-
+    @ConfigItem(
+            keyName = "holdShift",
+            name = "Hold Shift",
+            description = "Configures whether you need to hold shift to inspect equipment",
+            position = 1
+    )
+    default boolean holdShift()
+    {
+        return false;
+    }
 }
