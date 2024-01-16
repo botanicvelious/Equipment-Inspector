13d20b38c1ddec3c4cfe9213c5cb25d133e01a93
Waistcoat <Waistcoat@github.com>
2024-01-15 18:20:01 -0700

    Adding color and number formatting for prices

--- src/main/java/equipmentinspector/ItemPanel.java
+++ src/main/java/equipmentinspector/ItemPanel.java
@@ -2,26 +2,28 @@ package equipmentinspector;
 
 import net.runelite.api.ItemComposition;
 import net.runelite.api.kit.KitType;
-import net.runelite.client.plugins.playerindicators.PlayerNameLocation;
 import net.runelite.client.util.AsyncBufferedImage;
 import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.FontManager;
 import net.runelite.client.util.LinkBrowser;
+import net.runelite.client.util.QuantityFormatter;
 import okhttp3.HttpUrl;
 import org.apache.commons.lang3.StringUtils;
 
-import javax.swing.*;
+import javax.swing.GroupLayout;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.PopupMenuEvent;
-import javax.swing.event.PopupMenuListener;
+import java.awt.Color;
 import java.text.NumberFormat;
 import java.util.Locale;
-import java.util.concurrent.atomic.AtomicLong;
 
 class ItemPanel extends JPanel
 {
 
-    ItemPanel(ItemComposition item, KitType kitType, AsyncBufferedImage icon, String itemPrice)
+    ItemPanel(ItemComposition item, KitType kitType, AsyncBufferedImage icon, Integer itemPrice)
     {
         setBorder(new EmptyBorder(3, 3, 3, 3));
         setBackground(ColorScheme.DARK_GRAY_COLOR);
@@ -49,9 +51,7 @@ class ItemPanel extends JPanel
 
         JLabel location = new JLabel(StringUtils.capitalize(kitType.toString().toLowerCase()));
         location.setFont(FontManager.getRunescapeSmallFont());
-
-        JLabel price = new JLabel(StringUtils.capitalize(itemPrice.toLowerCase()));
-        location.setFont(FontManager.getRunescapeSmallFont());
+        JLabel price = getPriceLabel(itemPrice);
 
         JLabel imageLabel = new JLabel();
         icon.addTo(imageLabel);
@@ -79,36 +79,50 @@ class ItemPanel extends JPanel
                         .addComponent(location)
                         .addComponent(price)
                 )
-
         );
-
 
         // AWT's Z order is weird. This put image at the back of the stack
         setComponentZOrder(imageLabel, getComponentCount() - 1);
         setComponentPopupMenu(popupMenu);
     }
-
+    public static JLabel getPriceLabel(long amount) {
+        String itemPriceString = QuantityFormatter.quantityToStackSize(amount);
+        JLabel price = new JLabel(StringUtils.capitalize(itemPriceString));
+        if (amount > 10000000) {
+            price.setForeground(Color.GREEN);
+        } else if (amount > 100000) {
+            price.setForeground(Color.WHITE);
+        } else if (amount == 0) {
+            price.setForeground(Color.LIGHT_GRAY);
+        } else {
+            price.setForeground(Color.YELLOW);
+        }
+        price.setFont(FontManager.getRunescapeFont());
+        price.setToolTipText(NumberFormat.getNumberInstance(Locale.US).format(amount));
+        return price;
+    }
 }
 class TotalPanel extends JPanel {
 
-    TotalPanel(AtomicLong total) {
+    TotalPanel(long total) {
         setBorder(new EmptyBorder(3, 3, 3, 3));
         setBackground(ColorScheme.DARK_GRAY_COLOR);
 
         GroupLayout layout = new GroupLayout(this);
         this.setLayout(layout);
 
-        JLabel name = new JLabel("Total: " + NumberFormat.getNumberInstance(Locale.US).format(total));
+        JLabel name = new JLabel("Total: ");
+        JLabel price = ItemPanel.getPriceLabel(total);
         name.setFont(FontManager.getRunescapeFont());
 
         layout.setVerticalGroup(layout.createParallelGroup()
                 .addComponent(name)
-
+                .addComponent(price)
         );
         layout.setHorizontalGroup(layout.createSequentialGroup()
                 .addGap(8)
                 .addComponent(name)
-
+                .addComponent(price)
         );
     }
 }
